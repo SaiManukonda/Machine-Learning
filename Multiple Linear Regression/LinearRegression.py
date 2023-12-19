8ac72b500452c5401a21093ac49646bac3feb3e8
SaiManukonda <SaiManukonda24@gmail.com>
2023-12-18 21:02:31 -0500

    multiple Linear regression file created

---
+++ Multiple Linear Regression/LinearRegression.py
@@ -0,0 +1,14 @@
+
+class LinearRegression:
+    
+    #initialize this object with the training and testing data
+    def __init__(self):
+        pass
+    
+    def train(self, learningRate, iterations):
+        pass
+    
+    #prints the predicted and actual model
+    def check(self):
+        pass
+                
